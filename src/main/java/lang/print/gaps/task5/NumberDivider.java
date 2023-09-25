4bc7611bab42e6e7e3c66fadd5c7298a4eb26b69
Igor Chvalun <igor.chvalun@investengine.com>
2023-09-25 16:08:38 +0530

    Dividers

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        int d = 5;
+        double devi = (double)toBeDivided / d;
+        System.out.print(devi);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float hours = minutes * 60;
+        System.out.print(hours);
     }
 }
